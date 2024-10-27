ff28eb81f4db74544e77a10e34fec8a7ce4d4523
Lauro Guedes <leowgweb@gmail.com>
2024-10-27 11:37:58 +0000

    feat(ui): Install daysy UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ config('app.name', 'Wallai - Create your own phone wallpaper') }}</title>
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body>
+    <body class="min-h-screen antialiased text-[#242e42]">
         {{ $slot }}
     </body>
 </html>
